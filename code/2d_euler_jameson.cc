16cae869c9058444990db58797d866ea01fcfb5e
crewsdw <dcrews@uw.edu>
2017-03-14 22:44:33 -0700

    Fixed normals, reading/thinking about BCs

--- code/2d_euler_jameson.cc
+++ code/2d_euler_jameson.cc
@@ -27,10 +27,14 @@ void readGrid(std::vector<std::vector<double> > &grid_x,
     std::ifstream y_file("grid_y.dat");
     
     float x, y;
-    for (int j=0; j<J_max;j++) {
-        for (int i=0; i<I_max;i++) {
+    for (int j = 0; j < J_max; j++) {
+        for (int i = 0; i < I_max; i++) {
             x_file >> x;
             y_file >> y;
+	    if (i == 0) {
+		std::cout << "x is" << x << "\n";
+		std::cout << "y is" << y << "\n";
+	    }
             grid_x[i][j] = x;
             grid_y[i][j] = y;
         }
@@ -47,13 +51,12 @@ void createCellCentPoints(  std::vector<std::vector<double> > &x,
     int I_max = x.size();
     int J_max = x[0].size();
     
-    for (int i=0; i<I_max;i++) {
-         for (int j=0; j<J_max;j++) {
+    for (int i = 0; i < I_max; i++) {
+         for (int j = 0; j < J_max; j++) {
             x[i][j] = 0.25*(grid_x[i][j] + grid_x[i+1][j] + grid_x[i][j+1] + grid_x[i+1][j+1]);
             y[i][j] = 0.25*(grid_y[i][j] + grid_y[i+1][j] + grid_y[i][j+1] + grid_y[i+1][j+1]);
         }
     }
-
     
     // add ghost cells
 }
@@ -69,20 +72,20 @@ void writeCellCentPoints(const std::vector<std::vector<double> > &x,
     std::ofstream x_file("output/cell_x.dat");
     std::ofstream y_file("output/cell_y.dat");
     
-    for (int i=0; i<I_max;i++) {
-         for (int j=0; j<J_max;j++) {
+    for (int i=0; i < I_max;i++) {
+         for (int j=0; j < J_max;j++) {
             x_file << x[i][j] << std::endl;
             y_file << y[i][j] << std::endl;
         }
     }
 }
 
+
 void computeCellAreas(std::vector<std::vector<double> > &omega,
                         const std::vector<std::vector<double> > grid_x,
-                        const std::vector<std::vector<double> > grid_y
-                        )
+                        const std::vector<std::vector<double> > grid_y)
 {
-    // Uses grid points to calculate cell areas by takeing cross product of diags
+    // Uses grid points to calculate cell areas by taking cross product of diags
     // Indexing of areas matches cell center points
     int I_max = omega.size();
     int J_max = omega[0].size();
@@ -115,17 +118,17 @@ void computeCellNormals(std::vector<std::vector<double> > &dsi_x,
     int I_max = dsi_x.size();
     int J_max = dsi_x[0].size();
     
-    for (int i=0; i<I_max;i++) {
-        for (int j=0; j<J_max;j++) {
-            dsi_x[i][j] = grid_y[i+1][j] - grid_y[i][j];
-            dsi_y[i][j] = grid_x[i+1][j] - grid_x[i][j];
-            dsj_x[i][j] = grid_y[i][j+1] - grid_y[i][j];
-            dsj_y[i][j] = grid_x[i][j+1] - grid_x[i][j];
+    for (int i = 0; i < I_max; i++) {
+        for (int j = 0; j < J_max; j++) {
+	  dsi_x[i][j] = (grid_y[i+1][j] - grid_y[i][j]);
+	  dsi_y[i][j] = -1.0*(grid_x[i+1][j] - grid_x[i][j]); // Modified w/ -1
+	  dsj_x[i][j] = (grid_y[i][j+1] - grid_y[i][j]);
+	  dsj_y[i][j] = -1.0*(grid_x[i][j+1] - grid_x[i][j]); // Modified w/ -1
         }
     }
 }
 
-void setIC(std::vector<std::vector<std::vector<double> > > &u,
+void setIC(std::vector< std::vector< std::vector<double> > > &u,
             const double rho_0, const double u_0, const double v_0, 
             const double E_0)
 {
@@ -152,7 +155,7 @@ void writeOutput(const std::vector<std::vector<std::vector<double> > > &u)
     // Write out all variables
     int I_max = u[0].size();
     int J_max = u[0][0].size();
-
+    
     std::ofstream rho   ("output/rho.dat");
     std::ofstream rho_u ("output/rho_u.dat");
     std::ofstream rho_v ("output/rho_v.dat");
@@ -182,7 +185,7 @@ int main()
 {
     // Run parameters
     double cfl      = 2.8;
-    double R_min    = 0.000001; // Target residual value
+    double R_min    = 1.0e-6; // Target residual value
     double vars     = 4; // Number of conserved variables
     
     // Free stream values (used in ICs and BCs)
@@ -193,30 +196,32 @@ int main()
     double c_0      = 300.;
     double E_0      = 193.;
     
-    // Allocate all global vectors that will be used throughout sim
+    ////// Allocate all global vectors that will be used throughout sim
     int N_row = 129;
     int N_col = 65;
     // Grid points at cell verticies
-    std::vector<std::vector<double> > grid_x;
-    std::vector<std::vector<double> > grid_y;
+    std::vector< std::vector<double> > grid_x;
+    std::vector< std::vector<double> > grid_y;
     // Grid points at cell centers
-    std::vector<std::vector<double> > x;
-    std::vector<std::vector<double> > y;
+    std::vector< std::vector<double> > x;
+    std::vector< std::vector<double> > y;
     // Cell areas
-    std::vector<std::vector<double> > omega;
+    std::vector< std::vector<double> > omega;
     // Cell wall normals
-    std::vector<std::vector<double> > dsi_x;
-    std::vector<std::vector<double> > dsi_y;
-    std::vector<std::vector<double> > dsj_x;
-    std::vector<std::vector<double> > dsj_y;
+    std::vector< std::vector<double> > dsi_x;
+    std::vector< std::vector<double> > dsi_y;
+    std::vector< std::vector<double> > dsj_x;
+    std::vector< std::vector<double> > dsj_y;
     // Conserved variables
-    std::vector<std::vector<std::vector<double> > > u;
-    // Size the vectors
+    std::vector< std::vector< std::vector<double> > > u;
+    
+    ////// Size the vectors
     std::vector<double> col (N_col); // For vectors that hold verticie values
     std::vector<double> colm (N_col-1); // For vectors that hold cell centered values
     grid_x.push_back(col);
     grid_y.push_back(col);
-    for (int row=0; row<N_row-1; row++) {
+    
+    for (int row = 0; row < N_row - 1; row++) {
         grid_x.push_back(col);
         grid_y.push_back(col);
         x.push_back(colm);
@@ -227,7 +232,8 @@ int main()
         dsj_x.push_back(colm);
         dsj_y.push_back(colm);
     }
-    for (int var=0;var<vars;var++){
+    
+    for (int var = 0; var<vars; var++){
         u.push_back(x);
     }
     
@@ -248,6 +254,8 @@ int main()
     writeOutput(u);
     
     // Run sim
+    
+    
     
     return 0;
 }
--- code/grid_plot.py
+++ code/grid_plot.py
@@ -2,6 +2,7 @@
 AA543 - CFD, HW#5, Winter 2017
 
 Plots grids
+Right now, plots cell centers and connects as grid
 """
 
 from matplotlib import pyplot as plt
@@ -16,23 +17,24 @@ x = np.zeros((I_max, J_max))
 y = np.zeros((I_max, J_max)) 
 
 i = 0
-j=0
-for line in open('cell_x.dat', 'r'):
+j = 0
+for line in open('output/cell_x.dat', 'r'):
     if j < 63 :
         x[i,j] = float(line)
-        j = j +1
+        j = j + 1
     else:
         x[i,j] = float(line)
-        #~ print(x[0,j], ", ", x[-1,j]
+        #~ print(x[0,j], ", ", x[-1,j])
         j = 0
         i = i + 1
-        
+
 i = 0
 j = 0
-for line in open('cell_y.dat', 'r'):
-    if j < 63 :
+
+for line in open('output/cell_y.dat', 'r'):
+    if j < 63:
         y[i,j] = float(line)
-        j = j +1
+        j = j + 1
     else:
         y[i,j] = float(line)
         #~ print(y[0,j], ", ", y[-1,j])
@@ -45,6 +47,8 @@ for r in range(len(x)):
     plt.plot(x[r,:], y[r,:], 'g')
 for c in range(len(x[0])):
     plt.plot(x[:,c], y[:,c], 'r')
+
+
 plt.xlabel('x')
 plt.ylabel('y')
 #~ plt.xlim([-1,2])
